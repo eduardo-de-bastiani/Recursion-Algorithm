ecad3954d175a471fe02fe565caffc944d91fdcd
Eduardo De Bastiani <eduardocolladebastiani@gmail.com>
2024-09-14 17:14:54 -0300

    adicionando inicio logica transformaString

--- saltos.java
+++ saltos.java
@@ -1,10 +1,20 @@
 public class saltos{
 
+    public static int trasformaString(String in){
+        int n;
+
+
+
+        return n;
+    }
+
     public static int R(int n){
         if(n == 0) return 1;
         if(n == 1) return 1;
         if(n == 2) return 2;
         
+        //if(tamanho da string sem ser transformada >= 6) return R(n-1) + R(n-2) + R(n-3) - R(n-6);
+
         if(n >= 6) return R(n-1) + R(n-2) + R(n-3) - R(n-6);
 
         return R(n-1) + R(n-2) + R(n-3);
@@ -19,8 +29,9 @@ public class saltos{
         
         String in = args[0];
 
-        int n = Integer.valueOf(in);
-
+        // int n = Integer.valueOf(in);
+        
+        trasformaString(in);
         R(n);
         printR(n);
         
